f74b55838c7f12e7148dc37a2d788adb85d32a5b
carlilek <carlilek@janelia.hhmi.org>
2017-05-30 14:30:32 -0400

    checkforupdate should work now.

--- spark-janelia-lsf.py
+++ spark-janelia-lsf.py
@@ -228,6 +228,7 @@ def checkforupdate():
     scriptdir = os.path.dirname(os.path.realpath(__file__))
     os.chdir(scriptdir)
     output = subprocess.check_output('git fetch --dry-run', shell=True) 
+    print output
     if "origin" in output: 
         reply = raw_input("This script is not up to date. Would you like to update now? (y/n) ")
         if reply == 'y':
