0fa29b63e8cac5ddab6838b77938dd328017ae81
Eric Trautman <trautmane@janelia.hhmi.org>
2023-07-12 13:03:44 -0400

    Add -V option to all qsub calls so that environment variables from the submission shell are passed to scheduled jobs.  Hope is that this allows qstat calls in 11-verify... and 12-shutdown... scripts to work without explicitly referencing/loading institute specific paths like /etc/profile.d/gridengine.sh.

--- templates/scripts/10-queue-sge-jobs.sh
+++ templates/scripts/10-queue-sge-jobs.sh
@@ -36,7 +36,7 @@ echo "$(date) [${HOSTNAME}] submitting jobs to scheduler"
 
 qsub @{common_job_args} -l h_rt=@{job_max_run_seconds} \
   -N ${MASTER_JOB_NAME} -pe mpi 1 \
-  -j y -o "${MASTER_LOG}" \
+  -j y -V -o "${MASTER_LOG}" \
   "@{run_scripts_dir}/01-launch-master.sh"
 
 # SGE does not support "started" job dependencies, so start polling master log right away and poll for longer
@@ -45,7 +45,7 @@ MAX_POLL_SECONDS=$(( (MAX_ATTEMPTS + 1) * 30 ))
 
 qsub @{common_job_args} -l h_rt=${MAX_POLL_SECONDS} \
   -N ${URL_JOB_NAME} -pe mpi 1 \
-  -j y -o "@{run_logs_dir}/02-url.log" \
+  -j y -V -o "@{run_logs_dir}/02-url.log" \
   "@{run_scripts_dir}/02-save-master-url.sh" "${MASTER_LOG}" ${MAX_ATTEMPTS}
 
 # SGE does not support "started" job dependencies, so start polling qstat as soon as master URL is saved
@@ -53,25 +53,25 @@ qsub @{common_job_args} -l h_rt=${MAX_POLL_SECONDS} \
 POLLING_INTERVAL_SECONDS=30
 qsub @{common_job_args} -l h_rt=@{job_max_run_seconds} \
   -N ${WORKER_READY_JOB_NAME} -pe mpi 1 \
-  -j y -o "@{run_logs_dir}/11-verify-workers-ready.log" \
+  -j y -V -o "@{run_logs_dir}/11-verify-workers-ready.log" \
   "@{run_scripts_dir}/11-verify-sge-workers-ready.sh" \
   ${WORKER_JOB_NAME} ${MAX_ATTEMPTS} ${POLLING_INTERVAL_SECONDS} @{min_worker_nodes}
 
 # include $TASK_ID in worker log name so that output from multiple workers are separated
 qsub @{common_job_args} -l h_rt=@{job_max_run_seconds} \
   -N ${WORKER_JOB_NAME} -t 1-@{max_worker_nodes} -hold_jid ${URL_JOB_NAME} -pe mpi @{worker_slots} \
-  -j y -o "@{run_logs_dir}/worker-\$TASK_ID-launch.log" \
+  -j y -V -o "@{run_logs_dir}/worker-\$TASK_ID-launch.log" \
   "@{run_scripts_dir}/03-launch-worker.sh"
 
 qsub @{common_job_args} -l h_rt=@{job_max_run_seconds} \
   -N ${DRIVER_JOB_NAME} -hold_jid ${WORKER_READY_JOB_NAME}  -pe mpi @{driver_slots} \
-  -j y -o "${DRIVER_LOG}" \
+  -j y -V -o "${DRIVER_LOG}" \
   "@{run_scripts_dir}/04-launch-driver.sh"
 
 # do not redirect shutdown job output so that it gets emailed instead (notifying user of completion)
 qsub @{common_job_args} -l h_rt=1800 \
   -N ${SHUTDOWN_JOB_NAME} -hold_jid ${DRIVER_JOB_NAME} -pe mpi 1 \
-  -j y \
+  -j y -V \
   "@{run_scripts_dir}/12-shutdown-sge-jobs.sh" "${DRIVER_LOG}" ${REVERSE_ORDERED_JOB_NAMES}
 
 JOB_NAME_PREFIX="@{job_name_prefix}"
